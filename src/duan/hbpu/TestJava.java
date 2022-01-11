7138114783b75196070ea4bce97134f9962ee9d1
duanyujie <2981386671@qq.com>
2022-01-11 23:15:58 +0800

    测试

---
+++ src/duan/hbpu/TestJava.java
@@ -0,0 +1,16 @@
+package duan.hbpu;
+
+/**
+ * @ProjectName: wzxy
+ * @Package: duan.hbpu
+ * @ClassName: TestJava
+ * @Author: Admin
+ * @Description:
+ * @Date: 2022/1/11 23:15
+ * @Version: 1.0
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("测试成功");
+    }
+}
